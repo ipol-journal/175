4ee180f6dd4d9fde5bb8f640edb681e3eb7dfb16
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2023-03-22 13:14:07 +0530

    convert input image to pgm

--- run.py
+++ run.py
@@ -2,10 +2,15 @@
 
 import subprocess
 import sys
+import cv2
+
+#convert input image into pgm
+im = cv2.imread("input_0.png")
+img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("input_0.pgm",img)
 
 # run Smooth Contours
-p1 = subprocess.run(['smooth_contours','input_0.png', '-p','output.pdf','-t','output.txt'])
-
+p1 = subprocess.run(['smooth_contours','input_0.pgm', '-p','output.pdf','-t','output.txt'])
 
 # convert the PDF result into a PNG image
 p2 = subprocess.run(['gs', '-dNOPAUSE', '-dBATCH', '-sDEVICE=pnggray', '-dGraphicsAlphaBits=4',
